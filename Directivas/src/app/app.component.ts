243cb56c7f3f4b7f3d0ceec3cad0d94aba9e6425
iamjoselyn <jossy.x3@hotmail.com>
2021-07-01 13:42:08 +0200

    ngStyle directive to change the html style

---
+++ Directivas/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Directivas';
+
+  propiedadesParrafo: any;
+
+  constructor(){
+    this.propiedadesParrafo = {
+      color: 'red',
+      fontSize: '24px'
+    }
+  }
+
+  onClick(pColor:string){
+    switch (pColor) {
+      case 'y':
+        this.propiedadesParrafo.color = "yellow";
+        break;
+      case 'g':
+        this.propiedadesParrafo.color = "green";
+        break;
+      case 'r' :
+        this.propiedadesParrafo.color = "red";
+        break;
+    }
+  }
+  
+  // Se coge el valor cuando soltamos el click
+  onChange($event){
+    this.propiedadesParrafo.fontSize = `${$event.target.value}px`;
+  }
+  
+  // Se coge el valor en vivo
+  onInput($event){
+    this.propiedadesParrafo.fontSize = `${$event.target.value}px`;
+  }
+
+}
